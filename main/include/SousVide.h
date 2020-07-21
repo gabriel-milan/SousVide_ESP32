339b958617af7e4a503f76226a120841609b6179
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2020-07-21 10:00:33 -0300

    Major fix on imports

--- main/include/SousVide.h
+++ main/include/SousVide.h
@@ -10,9 +10,11 @@
 // BLE stuff
 #define DEVICE_NAME                       "SousVide"
 #define SERVICE_UUID                      "90f72f6e-6e9a-459d-b21b-8a2d91039b52"
-#define CHARACTERISTIC_UUID               "68b44d72-e082-4581-bc29-d4c5ed732476"
-#define INITIAL_CHARACTERISTIC_VALUE      "N/A"
-#define INSTALLATION_CHARACTERISTIC_VALUE "0000"
+#define COOKING_TIME_CHARACTERISTIC_UUID  "68b44d72-e082-4581-bc29-d4c5ed732476"
+#define COOKING_TEMP_CHARACTERISTIC_UUID  "e4c3be56-fb34-49b6-82c5-b8cba058c223"
+#define ENABLE_CHARACTERISTIC_UUID        "cd5399c9-05a3-473a-8464-3a64b54bc46f"
+#define TIME_PASSED_CHARACTERISTIC_UUID   "7f27daa0-f648-43c7-9dbc-0a81bcc83ff4"
+#define HEATING_EN_CHARACTERISTIC_UUID    "ad4d945b-9a6e-4792-8bf4-f7097352c9c8"
 // MultiCore stuff
 #define CORE_ZERO                         0
 #define CORE_ONE                          1
